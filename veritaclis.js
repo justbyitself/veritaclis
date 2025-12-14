bb6cde76d2ea4fc29f1f1017a9e839f036417bcd
justbyitself <justbyitself@proton.me>
2025-12-14 11:31:43 +0100

    Refactor veritaclis.js to keep runPreconditions and runPostconditions private

--- veritaclis.js
+++ veritaclis.js
@@ -1,5 +1,30 @@
 import * as command from "./command.js"
 
+async function runPreconditions(preList) {
+  const results = []
+  for (const { description, check } of preList || []) {
+    const passed = await check()
+    results.push({ description, passed })
+    if (!passed) {
+      break
+    }
+  }
+  return results
+}
+
+function runPostconditions(postList, commandResult) {
+  const results = []
+  for (const { description, check } of postList || []) {
+    const passed = check({
+      stdout: commandResult.stdout,
+      exitCode: commandResult.exitCode,
+      success: commandResult.success,
+    })
+    results.push({ description, passed })
+  }
+  return results
+}
+
 async function runTest(testFile) {
   const result = {
     path: testFile,
@@ -12,25 +37,16 @@ async function runTest(testFile) {
     const mod = await import(`./${testFile}`)
     const testDef = mod.default
 
-    for (const { description, check } of testDef.pre || []) {
-      const passed = await check()
-      result.pre.push({ description, passed })
-      if (!passed) {
-        return result
-      }
+    result.pre = await runPreconditions(testDef.pre)
+
+    if (result.pre.some(p => !p.passed)) {
+      return result
     }
 
     const { command: cmdName, args } = testDef.run()
     const commandResult = await command.run(cmdName, args)
 
-    for (const { description, check } of testDef.post || []) {
-      const passed = check({
-        stdout: commandResult.stdout,
-        exitCode: commandResult.exitCode,
-        success: commandResult.success,
-      })
-      result.post.push({ description, passed })
-    }
+    result.post = runPostconditions(testDef.post, commandResult)
 
     return result
 
