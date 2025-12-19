88dda54aa38613f02c4870d79626087830440cb9
justbyitself <justbyitself@proton.me>
2025-12-19 12:53:23 +0100

    Move modules to src/ and refactor runners and utilities

--- runners/index.js
+++
@@ -1,3 +0,0 @@
-export { default as pre } from './pre.js'
-export { default as command } from './command.js'
-export { default as post } from './post.js'
--- runners/post.js
+++
@@ -1,8 +0,0 @@
-export default function(postList, context) {
-  const results = []
-  for (const { description, check } of postList || []) {
-    const passed = check(context)
-    results.push({ description, passed })
-  }
-  return results
-}
