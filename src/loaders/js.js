0e01fa2d9ec08957fac80616ed0ddd6d747b1bcf
justbyitself <justbyitself@proton.me>
2025-12-21 21:26:40 +0100

    Fix dynamic imports of local test files in JSR environment

--- src/loaders/js.js
+++ src/loaders/js.js
@@ -1,1 +1,3 @@
-export default async (filePath) => (await import('file://' + filePath)).default
+import { importModule } from "jsr:@brad-jones/jsr-dynamic-imports@^0.1.2"
+
+export default async (filePath) => (await importModule('file://' + filePath)).default
